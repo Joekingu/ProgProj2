88ab6d234aa025c4abc71846cd31cdefc2247cd3
Peio <peio.parquic@etudiant.univ-rennes1.fr>
2023-05-17 00:09:23 +0200

    SONS

---
+++ src/entity/Songs.java
@@ -0,0 +1,29 @@
+package entity;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Songs {
+	
+	String m_path;
+	
+	public Songs(String path) {
+		m_path = path;
+	}
+
+	public void play() {
+		try {
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(getClass().getResource(m_path));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			clip.start();
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+}
