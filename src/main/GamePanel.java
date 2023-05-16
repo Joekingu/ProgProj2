7e94c5e72f5fcac9f764899cd8f82f99a8cfc395
RHOo22 <2711a.rault@gmail.com>
2023-05-16 16:00:22 +0200

    debut game over

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,21 +20,22 @@ import java.util.ArrayList;
  * Panel principal du jeu contenant la map principale
  *
  */
-public class GamePanel extends JPanel implements Runnable{
-	
-	//Param�tres de l'�cran
-	final int ORIGINAL_TILE_SIZE = 1; 							// une tuile de taille 16x16
-	public final int SCALE = 60; 										// �chelle utilis�e pour agrandir l'affichage
-	public final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; 	// 48x48
+public class GamePanel extends JPanel implements Runnable {
+
+	// Param�tres de l'�cran
+	final int ORIGINAL_TILE_SIZE = 1; // une tuile de taille 16x16
+	public final int SCALE = 60; // �chelle utilis�e pour agrandir l'affichage
+	public final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; // 48x48
 	public final int MAX_SCREEN_COL = 10;
-	public final int MAX_SCREE_ROW = 10; 					 	// ces valeurs donnent une r�solution 4:3
+	public final int MAX_SCREE_ROW = 10; // ces valeurs donnent une r�solution 4:3
 	public final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 768 pixels
-	public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREE_ROW;	// 576 pixels
+	public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREE_ROW; // 576 pixels
 
 	// FPS : taux de rafraichissement
 	int m_FPS;
-	
-	// Cr�ation des diff�rentes instances (Player, KeyHandler, TileManager, GameThread ...)
+
+	// Cr�ation des diff�rentes instances (Player, KeyHandler, TileManager,
+	// GameThread ...)
 	KeyHandler m_keyH;
 	KeyHandler m_keyH2;
 	Thread m_gameThread;
@@ -45,90 +46,94 @@ public class GamePanel extends JPanel implements Runnable{
 	Camera m_camera;
 	ArrayList<Collectable> acollecter;
 	ArrayList<Entity> listEntity;
-		
+
 	/**
 	 * Constructeur
 	 */
 	public GamePanel() {
-		m_FPS = 60;				
+		m_FPS = 60;
 		m_keyH = new KeyHandler();
 		m_keyH2 = new KeyHandler();
 		m_player = new Player(this, m_keyH, m_ammo);
-		m_ammo = new Ammo(this, m_keyH2, m_player, 10, 10, TILE_SIZE/4);
+		m_ammo = new Ammo(this, m_keyH2, m_player, 10, 10, TILE_SIZE / 4);
 		m_tileM = new TileManager(this);
-		m_pnj = new pnj(this,50);
+		m_pnj = new pnj(this, 50);
 		m_camera = new Camera(m_player);
 		listEntity = new ArrayList<>();
 		listEntity.add(m_pnj);
-		
+
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 		this.addKeyListener(m_keyH);
 		this.setFocusable(true);
 	}
-	
+
 	/**
 	 * Lancement du thread principal
-	 * @return 
+	 * 
+	 * @return
 	 */
-	
+
 	public Entity getPlayer() {
 		return m_player;
 	}
-	
+
 	public ArrayList<Entity> getListEntity() {
 		return listEntity;
 	}
-	
+
 	public void startGameThread() {
 		m_gameThread = new Thread(this);
 		m_gameThread.start();
 	}
-	
+
 	public void run() {
-		
-		double drawInterval = 1000000000/m_FPS; // rafraichissement chaque 0.0166666 secondes
-		double nextDrawTime = System.nanoTime() + drawInterval; 
-		
-		while(m_gameThread != null) { //Tant que le thread du jeu est actif
-			
-			//Permet de mettre � jour les diff�rentes variables du jeu
-			this.update();
-			
-			//Dessine sur l'�cran le personnage et la map avec les nouvelles informations. la m�thode "paintComponent" doit obligatoirement �tre appel�e avec "repaint()"
+
+		double drawInterval = 1000000000 / m_FPS; // rafraichissement chaque 0.0166666 secondes
+		double nextDrawTime = System.nanoTime() + drawInterval;
+
+		while (m_gameThread != null) { // Tant que le thread du jeu est actif
+
+			// Permet de mettre � jour les diff�rentes variables du jeu
+			if (m_player.isAlive()) {
+				this.update();
+			}
+
+			// Dessine sur l'�cran le personnage et la map avec les nouvelles informations.
+			// la m�thode "paintComponent" doit obligatoirement �tre appel�e avec
+			// "repaint()"
 			this.repaint();
-			
-			//Calcule le temps de pause du thread
+
+			// Calcule le temps de pause du thread
 			try {
 				double remainingTime = nextDrawTime - System.nanoTime();
-				remainingTime = remainingTime/1000000;
-				
-				if(remainingTime < 0) {
+				remainingTime = remainingTime / 1000000;
+
+				if (remainingTime < 0) {
 					remainingTime = 0;
 				}
-				
-				Thread.sleep((long)remainingTime);
+
+				Thread.sleep((long) remainingTime);
 				nextDrawTime += drawInterval;
-				
+
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	
 
 	/**
 	 * Mise � jour des donn�es des entit�s
 	 */
 	public void update() {
 		m_player.update();
-		m_pnj.update(m_player.getx(),m_player.gety());
+		m_pnj.update(m_player.getx(), m_player.gety());
 		m_camera.update(this);
 		m_ammo.update();
 	}
-	
+
 	/**
 	 * Affichage des �l�ments
 	 */
@@ -142,7 +147,7 @@ public class GamePanel extends JPanel implements Runnable{
 		m_player.draw(g2);
 		g2.dispose();
 	}
-	
+
 	public TileManager gettileM() {
 		return m_tileM;
 	}
