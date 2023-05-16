db7b510b294c03fe44ce8eea5fb5a5990594da01
Peio <peio.parquic@etudiant.univ-rennes1.fr>
2023-05-16 21:08:13 +0200

    Marche sur Matériaux

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -111,25 +111,45 @@ public class Player extends Entity {
 		}
 		return false;
 	}
-
-	private int testdeg() {
+//	private int testdeg() {
+//		int d = m_gp.TILE_SIZE;
+//		int d2 = d/2;
+//		int px = m_x+d2;
+//		int py = m_y+d2;
+//		if(m_gp.gettileM().map[(px+d2)/d][(py+d2)/d]==m_collision.lave) {
+//				return 2;
+//		}
+//		return 0;
+//	}
+	
+	private int testMat() {
 		int d = m_gp.TILE_SIZE;
-		int d2 = d / 2;
-		int px = m_x + d2;
-		int py = m_y + d2;
-		if (m_gp.gettileM().map[(px + d2) / d][(py + d2) / d] == m_collision.lave) {
+		int d2 = d/2;
+		int px = m_x+d2;
+		int py = m_y+d2;
+		if(m_gp.gettileM().map[(px+d2)/d][(py+d2)/d]==m_collision.snow) {
+			System.out.println("snaow");
+			m_speed=5;
+		}else if(m_gp.gettileM().map[(px+d2)/d][(py+d2)/d]==m_collision.sand) {
+			System.out.println("sand");
+			m_speed=1;
+		}else {
+			m_speed=2;
+		}
+		if(m_gp.gettileM().map[(px+d2)/d][(py+d2)/d]==m_collision.lave) {
 			return 2;
 		}
 		return 0;
 	}
 
 	public void update() {
-		ArrayList<Integer> pressed = m_keyH.boutons();
-		if (m_health <= 0) {
-			m_alive = false;
+		System.out.println(m_speed);
+		if (m_health<=0) {
+			m_alive=false;
 		}
-		if (testdeg() != 0) {
-			this.estblesse(testdeg());
+		testMat();
+		if(testMat()!=0) {
+			this.estblesse(testMat());
 		}
 		if (pressed.contains(Integer.valueOf(90)) && pressed.contains(Integer.valueOf(81))) {
 			if (!test(-2, 0)) {
